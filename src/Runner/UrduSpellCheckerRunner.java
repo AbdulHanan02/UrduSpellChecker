e5f30ae39c81508ab4f3c631e8f569d83e71ea4e
Sharif Ahmad <f200365@cfd.nu.edu.pk>
2022-11-21 18:38:35 +0500

    Generating Mutants and Storing in Database

--- src/Runner/UrduSpellCheckerRunner.java
+++ src/Runner/UrduSpellCheckerRunner.java
@@ -1,5 +1,6 @@
 package Runner;
 
+import businessLayer.MutantGenerator;
 import businessLayer.SeperateWords;
 import businessLayer.UrduSpellModel;
 import databaseLayer.FileToDatabase;
@@ -8,10 +9,12 @@ import presentationLayer.UrduSpellView;
 
 public class UrduSpellCheckerRunner {
 	public static void main(String[] args) {
-		FileToDatabase objD = new FileToDatabase();
-		objD.readAllFile();
-		SeperateWords objS = new SeperateWords();
-		objS.sepWords();
+		/*
+		 * FileToDatabase objD = new FileToDatabase(); objD.readAllFile(); SeperateWords
+		 * objS = new SeperateWords(); objS.sepWords();
+		 */
+		MutantGenerator mu = new MutantGenerator();
+		mu.GenerateAndStore();
 		UrduSpellView v = new UrduSpellView();
 		v.setVisible(true);
 		UrduSpellModel m = new UrduSpellModel();
